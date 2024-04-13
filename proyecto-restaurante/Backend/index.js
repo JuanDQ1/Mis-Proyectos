69ef7e89c29be9b0f4c5e4787610ec6a80f84b31
Juan David Quijano <juan.quijano01@unicatolica.edu.co>
2024-04-12 20:56:13 -0500

    base de datos conectada

--- proyecto-restaurante/Backend/index.js
+++ proyecto-restaurante/Backend/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const {urlencoded, json} = require('express');
 const router = require('./routes/rutas.js');
 const cors = require('cors');
+const mongoose = require('mongoose');
+require("dotenv").config();
 
 const app = express();
 
@@ -10,6 +12,12 @@ app.use(json())
 
 app.use(cors())
 app.use('/restaurante', router);
+
+//mongodb connection
+mongoose.connect(process.env.MONGODB_URL)
+.then(() => console.log('Conexion de base de datos'))
+.catch((error) => console.error(error));
+
 
 app.listen(3000, ()=>{
     console.log('listening at port 3000');
--- proyecto-restaurante/Backend/routes/controladores/restau.js
+++ proyecto-restaurante/Backend/routes/controladores/restau.js
@@ -6,7 +6,7 @@ const path = require('path');
 
 
 const consultarUsuario = async (req, res) => {
-    // console.log(req.body);
+    
 
     try {
         const usuarios = await fs.readFile(path.join(__dirname, '../../db/users.json'));
@@ -27,12 +27,7 @@ const consultarUsuario = async (req, res) => {
                 error: 'Credenciales inválidas'
             });
         }
-        // console.log(user);
-
-        // Aquí puedes realizar acciones adicionales si las credenciales son válidas,
-        // como generar un token de sesión, guardar información en una cookie, etc.
-
-        // Por ejemplo, si quieres devolver los datos del usuario:
+        
         res.json({
             status: 200,
             payload: user
