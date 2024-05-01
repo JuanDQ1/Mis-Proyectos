050a9ed0049d92000fce6461aa0a525ad59e5491
Juan David Quijano <juan.quijano01@unicatolica.edu.co>
2024-05-01 16:21:36 -0500

    CONEXION A BASE DE DATOS CON BACKEND
    LISTO

---
+++ Tiquetes-Avion/backend/routes/rutas.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const tiquetesControladores = require('./controladores/tique.js');
+router
+
+
+
+
+
+
+
+
+
+module.exports = router;
